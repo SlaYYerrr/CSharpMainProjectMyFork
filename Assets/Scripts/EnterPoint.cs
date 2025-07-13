802e8b90be2d68141a0d367c3ef3726da7531768
SlaYYerr <nikita.bredihin31@yandex.ru>
2025-07-13 12:51:58 +0300

    HomeWork1Coment

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,13 +11,14 @@ using Utilities;
 /*/ ûגאûגאûגאûגא
  * ûגאûגאûגא
  * dfgdfgdfgdfg
+ * ןנטגוע ןנמגונÿ‏שולף!
 /*/
 
 public class EnterPoint : MonoBehaviour
 {
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
-    private float _timeScale = 1;
+    private float _timeScale = 5;
     
     void Start()
     {
